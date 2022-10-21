12e7e0e09e20cc9f5e9f4404c6cf6b9989447630
Maria Camila Cubides Martin <macacumar97@gmail.com>
2022-10-21 14:22:09 -0500

    Fix redirect

--- round-robin/index.js
+++ round-robin/index.js
@@ -5,15 +5,15 @@ const { port, serviceUrl, servicePath } = require('./config/app');
 const { initializeRoundRobin, updateRoundRobin } = require('./src/roundRobin');
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
-app.get('/', async (req, res) => {
+app.post('*', async (req, res) => {
     const currentPort = global.Sequence[global.Count];
     updateRoundRobin();
-    res.redirect(`${serviceUrl}:${currentPort}/${servicePath}`)
+    res.redirect(307, `http://${serviceUrl}:${currentPort}/${servicePath}`)
 })
 
 function startRoundRobin() {
